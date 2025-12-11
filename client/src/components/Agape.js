a590b22e46ef90a47e933c1be814a75aa53967c7
DomiUmami <williams_dominik@yahoo.com>
2025-12-10 21:37:18 -0500

    -Change

--- client/src/components/Agape.js
+++ client/src/components/Agape.js
@@ -11,8 +11,8 @@ useEffect(() => {
     try {
       const res = await fetch("/api/saint");
       if (!res.ok) throw new Error("Failed to fetch monsters");
-      const data = await res.json();
       console.log(res)
+      const data = await res.json();
       setMonsters(data);
     } catch (err) {
       console.error("Monster fetch error:", err);
