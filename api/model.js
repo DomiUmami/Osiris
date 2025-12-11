21d33d28ceec237f480d94b7113262a910e2db5d
DomiUmami <williams_dominik@yahoo.com>
2025-12-10 19:33:37 -0500

    -Because it told me to

--- api/model.js
+++ api/model.js
@@ -1,6 +1,5 @@
 import { testData } from "./mockData.js";
 
-// For now, just return mockData
 export async function getAllSaintsMerged() {
-  return testData;
+  return testData; // always return mockData
 }
